70852ec70c14dd7744a1f6e48e9acc4f928c8fa2
Joshua Weekes <jweekes.business@gmail.com>
2025-12-30 15:20:43 -0400

    first_commit

---
+++ study_budy/base/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+rooms = [
+    {'id':1,'name':'Lets learn Python!'},
+    {'id':2,'name':'Design with me'},
+    {'id':3,'name':'Frontend Developers'},
+]
+
+# Create your views here.
+def home(request):
+    context = {'rooms':rooms}
+    return render(request,'home.html',context)
+
+def room(request):
+    return render(request,'room.html')
