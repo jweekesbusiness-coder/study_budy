82e210414299c284bf604d4760e0513c75e33d95
Joshua Weekes <jweekes.business@gmail.com>
2026-01-01 14:14:56 -0400

    new  forms&models

--- study_budy/base/views.py
+++ study_budy/base/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Room
+from .forms import RoomForm
 
 # rooms = [
 #     {'id':1,'name':'Lets learn Python!'},
@@ -15,10 +16,27 @@ def home(request):
     return render(request,'base/home.html',context)
 
 def room(request,pk):
-    room = None
-    for r in rooms:
-        if r['id'] == int(pk):
-            room = r
-            break
+    room = Room.objects.get(id=pk)
     context = {'room':room}
     return render(request,'base/room.html',context)
+
+def createRoom(request):
+    form =RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request,'base/room_form.html',context)
+
+def updateRoom(request,pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST,instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form':form}
+    return render(request,'base/room_form.html',context)
